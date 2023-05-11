296311ecf5c3c094f17ab1b60da62ac6284d02ee
unknown <SoloProgrammer>
2023-05-11 22:59:29 +0530

    first production commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+var cors = require('cors')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 8001
+
+const app = express();
+
+app.use(cors());
+
+app.listen(port, () => console.log(`Quiz-App is running on server...${port} `));
+
+// Deployment Setup!
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, './Frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'));
+});
